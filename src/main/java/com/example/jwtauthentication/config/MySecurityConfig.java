ffd43a0ece75232b85e0952efb043a8ee9fa255a
Gulab1102 <gk96025@gmail.com>
2023-08-20 23:57:04 +0530

    jwtauthentication

--- src/main/java/com/example/jwtauthentication/config/MySecurityConfig.java
+++ src/main/java/com/example/jwtauthentication/config/MySecurityConfig.java
@@ -3,7 +3,9 @@ package com.example.jwtauthentication.config;
 import org.springframework.boot.autoconfigure.info.ProjectInfoProperties.Build;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -15,7 +17,10 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class MySecurityConfig {
-	
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+	     return authenticationConfiguration.getAuthenticationManager();
+	}
 	@Bean
 	public UserDetailsService userDetailsService() {
 		return new CustomUserDetailServiceImpl();

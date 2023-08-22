0970e1e0d004fbc7e4d12a681a046b4a1b1a2a99
Gulab1102 <gk96025@gmail.com>
2023-08-22 12:48:49 +0530

    first commit

--- src/main/java/com/example/jwtauthentication/controller/HomeController.java
+++ src/main/java/com/example/jwtauthentication/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.example.jwtauthentication.controller;
 
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +12,7 @@ public class HomeController {
 
 	org.slf4j.Logger logger = LoggerFactory.getLogger(HomeController.class);
 
-	@RequestMapping("/test")
+	@PostMapping("/test")
 	public String test() {
 		this.logger.warn("This is working message");
 		return "{\"name\":\"Gulab\"}";
